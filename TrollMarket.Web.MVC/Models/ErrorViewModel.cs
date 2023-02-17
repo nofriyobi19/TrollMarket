254ae977ae11c84aa2f01d96a1634c9018772e61
LAPTOP-1RMCNNA4\1676 <nofriyobi52@gmail.com>
2023-02-17 17:00:29 +0700

    make a new project

---
+++ TrollMarket.Web.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace TrollMarket.Web.MVC.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
